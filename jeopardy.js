5fca3ee27b6470db6909e77c076795db69f06f13
Casey <casey@mba.lan>
2016-05-29 14:25:00 -0500

    added props and wp

--- jeopardy.js
+++ jeopardy.js
@@ -1,8 +1,15 @@
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topnums: 0
+    }
+  }
   render() {
     return (
       <div className="play-box">
         <h1>Jeopardy!</h1>
+        <h2>{this.props.topnums}</h2>
         <h4>Push the + symbol next to the selected amount of points if you answered
         correctly and first.</h4>
         <h4>Push the - symbol next to the selected amount of points if you answered
@@ -11,7 +18,8 @@ class Game extends React.Component {
         <h4>The "wager" button is only appropraite during Daily Doubles and
         Final Jeopardy.</h4>
         <br />
-        <button>-</button>100<button>+</button><br />
+        <button onClick={this.subtractScoreEen.bind(this)}>-</button>100
+        <button onClick={this.addScoreEen.bind(this)}>+</button><br />
         <button>-</button>200<button>+</button><br />
         <button>-</button>300<button>+</button><br />
         <button>-</button>400<button>+</button><br />
@@ -21,7 +29,75 @@ class Game extends React.Component {
         <button>-</button>1000<button>+</button>
       </div>
     );
+}
+  addScoreEen() {
+    this.setState({topnums: this.state.topnums += 100});
   }
+  subtractScoreEen() {
+    this.setState({topnums: this.state.topnums -= 100});
+  }
+  /*addScoreTwee() {
+
+  }
+  subtractScoreTwee() {
+
+  }
+  addScoreDrie() {
+
+  }
+  subtractScoreDrie() {
+
+  }
+  addScoreVier() {
+
+  }
+  subtractScoreVier() {
+
+  }
+  addScoreVijf() {
+
+  }
+  subtractScoreVijf() {
+
+  }
+  addScoreZes() {
+
+  }
+  subtractScoreZes() {
+
+  }
+  addScoreZeven() {
+
+  }
+  subtractScoreZeven() {
+
+  }
+  addScoreAcht() {
+
+  }
+  subtractScoreAcht() {
+
+  }
+  addScoreNegen() {
+
+  }
+  subtractScoreNegen() {
+
+  }
+  addScoreDuizend() {
+
+  }
+  subtractScoreDuizend() {
+
+  }
+
+var yourWinnings = {
+  totalScore: 0
 }
 
-ReactDOM.render(<Game />, document.getElementById('example'))
+console.log('Your score is'+yourWinnings.totalScore);
+*/
+}
+
+
+ReactDOM.render(<Game topnums={0}/>, document.getElementById('example'))
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8080',
+		'webpack/hot/only-dev-server',
+		'./jeopardy.js'
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	devServer: {
+		contentBase: "./public",
+			noInfo: true,
+			hot: true,
+			inline: true
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
+};
